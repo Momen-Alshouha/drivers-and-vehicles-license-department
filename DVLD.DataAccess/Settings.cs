969917236a76f9f64a03492fdbc9ff78c02dcf19
momen-alshouha <momenalshouha@outlook.com>
2024-06-04 14:54:40 +0300

    connection string added

---
+++ DVLD.DataAccess/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.DataAccess
+{
+    internal class Settings
+    {
+        public static readonly string ConnectionString = "Server=.;Database=ContactsDB;Integrated security=true;TrustServerCertificate=True";
+    }
+}
