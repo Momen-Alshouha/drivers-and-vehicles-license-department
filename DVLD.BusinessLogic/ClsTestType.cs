953a16bce8c2fe3a522fbdd75c71b55d12cff54b
momen-alshouha <momenalshouha@outlook.com>
2024-07-01 16:58:54 +0300

    business logic done for test types

---
+++ DVLD.BusinessLogic/ClsTestType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ClsDataType.ClsDataType;
+
+namespace DVLD.BusinessLogic
+{
+    public class ClsTestType
+    {
+        public static StTestType GetTestType(int id)
+        {
+            return DataAccess.ClsTestType.GetTestType(id);
+        }
+        public static DataTable GetAllTestTypes()
+        {
+            return  DataAccess.ClsTestType.GetTestTypes();
+        }
+        public static bool AddNew(StTestType testType)
+        {
+            return DataAccess.ClsTestType.AddTestType(testType);
+        }
+        public static bool Update(StTestType testType)
+        {
+            return DataAccess.ClsTestType.UpdateTestType(testType);
+        }
+
+    }
+}
