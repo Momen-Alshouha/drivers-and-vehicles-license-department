dd0287bb309dd3680683842639d3597cd4097557
momen-alshouha <momenalshouha@outlook.com>
2024-07-07 12:20:20 +0300

    StLocalDrivingLicenseApplication implemented to be tightly coupled with StApplicationData (composition)

---
+++ ClsDataType/ClsLocalDrivingLicenseApplications.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ClsDataType.ClsApplication;
+
+namespace ClsDataType
+{
+    public class ClsLocalDrivingLicenseApplications
+    {
+        public struct StLocalDrivingLicenseApplication
+        {
+            public int LocalDrivingLicenseApplicationID { get; set; }
+            public int ApplicationID { get; set; }
+            public int LicenseClassID { get; set; }
+            public StApplicationData ApplicationData { get; set; }
+
+            public StLocalDrivingLicenseApplication(
+                int localDrivingLicenseApplicationID,
+                int applicationID,
+                int licenseClassID,
+                StApplicationData applicationData)
+            {
+                LocalDrivingLicenseApplicationID = localDrivingLicenseApplicationID;
+                ApplicationID = applicationID;
+                LicenseClassID = licenseClassID;
+                ApplicationData = applicationData;
+            }
+        }
+    }
+}
