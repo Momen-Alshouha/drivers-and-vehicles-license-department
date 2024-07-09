a7e9f1f4e86669e1548bc628b78cb8c5dc347a27
momen-alshouha <momenalshouha@outlook.com>
2024-07-09 11:06:22 +0300

    GetConnection and GetCommand methods implemented in Settings class in DataAccess

--- DVLD.DataAccess/Settings.cs
+++ DVLD.DataAccess/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,16 @@ namespace DVLD.DataAccess
     internal class Settings
     {
         public static readonly string ConnectionString = "Server=.;Database=dvld;Integrated security=true;TrustServerCertificate=True";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public static SqlCommand GetCommand(string query)
+        {
+            return new SqlCommand(query, GetConnection());
+        }
     }
+
 }
