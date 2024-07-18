e13674ac107912c01deb6025eaa20a1705b3064d
momen-alshouha <momenalshouha@outlook.com>
2024-07-18 03:45:12 +0300

    StTestAppointment Struct declared in ClsDataType Layer

---
+++ ClsDataType/ClsTestAppintment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsDataType
+{
+    public class ClsTestAppintment
+    {
+        struct StTestAppointment
+        {
+            public int TestAppointmentID { get; set; }
+            public int TestTypeID { get; set; }
+            public int LocalDrivingLicenseApplicationID { get; set; }
+            public DateTime AppointmentDate { get; set; }
+            public decimal PaidFees { get; set; }
+            public int CreatedByUserID { get; set; }
+            public bool IsLocked { get; set; }
+            public int RetakeTestApplicationID { get; set; }
+        }
+    }
+}
