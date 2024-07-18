33be243a3f8079396a77c7ddb703f6149e5b4ba9
momen-alshouha <momenalshouha@outlook.com>
2024-07-18 04:34:27 +0300

    StTakenTest struct data type declared in ClsTakenTest in ClsDataType layer

---
+++ ClsDataType/ClsTakenTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsDataType
+{
+    public class ClsTakenTest
+    {
+        public struct StTakenTest
+        {
+            public int TestID { get; set; }
+            public int TestAppointmentID { get; set; }
+            public bool TestResult { get; set; }
+            public string Notes { get; set; }
+            public int CreatedByUserID { get; set; }
+        }
+
+    }
+}
