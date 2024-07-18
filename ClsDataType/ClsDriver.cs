c8cc70a93ea1d59c0da2ba99bafa9395062e602f
momen-alshouha <momenalshouha@outlook.com>
2024-07-18 05:29:46 +0300

    StLicense and StDriver structs data types implemented in ClsDataType layer

---
+++ ClsDataType/ClsDriver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsDataType
+{
+    public class ClsDriver
+    {
+        public struct StDriver
+        {
+            public int DriverID { get; set; }
+            public int PersonID { get; set; }
+            public int CreatedByUserID { get; set; }
+            public DateTime CreatedDate { get; set; }
+        }
+    }
+}
---
+++ ClsDataType/ClsLicense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ClsDataType.ClsApplication;
+
+namespace ClsDataType
+{
+    public class ClsLicense
+    {
+        public enum EnIssueReason
+        {
+            FirstTime = 1,
+            Renew,
+            ReplacementDamaged,
+            ReplacementLost
+        }
+
+        public struct StLicense
+        {
+            public int LicenseID { get; set; }
+            public int ApplicationID { get; set; }
+            public int DriverID { get; set; }
+            public EnLicenseClass EnLicenseClass { get; set; }
+            public DateTime IssueDate { get; set; }
+            public DateTime ExpirationDate { get; set; }
+            public string Notes { get; set; }
+            public decimal PaidFees { get; set; }
+            public bool IsActive { get; set; }
+            public EnIssueReason IssueReason { get; set; }
+            public int CreatedByUserID { get; set; }
+        }
+    }
+}
