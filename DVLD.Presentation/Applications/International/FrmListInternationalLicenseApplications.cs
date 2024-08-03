0bcaef620e32d34c03ed3b074faef4cdc2fa641d
momen-alshouha <momenalshouha@outlook.com>
2024-08-03 18:16:14 +0300

    some data grid view design modifications

--- DVLD.Presentation/Applications/International/FrmListInternationalLicenseApplications.cs
+++ DVLD.Presentation/Applications/International/FrmListInternationalLicenseApplications.cs
@@ -31,17 +31,17 @@ namespace DVLD.Presentation.Applications.International
         private void ModifyDataGridViewDesign()
         {
             dataGridViewInternationalApplications.Columns[0].HeaderText = "I.L ID";
-            dataGridViewInternationalApplications.Columns[0].Width = 50;
+            dataGridViewInternationalApplications.Columns[0].Width = 70;
             dataGridViewInternationalApplications.Columns[1].HeaderText = "App ID";
-            dataGridViewInternationalApplications.Columns[1].Width = 50;
+            dataGridViewInternationalApplications.Columns[1].Width = 70;
             dataGridViewInternationalApplications.Columns[2].HeaderText = "Driver ID";
-            dataGridViewInternationalApplications.Columns[2].Width = 50;
+            dataGridViewInternationalApplications.Columns[2].Width = 70;
             dataGridViewInternationalApplications.Columns[3].HeaderText = "L.L ID";
-            dataGridViewInternationalApplications.Columns[3].Width = 50;
+            dataGridViewInternationalApplications.Columns[3].Width = 70;
             dataGridViewInternationalApplications.Columns[4].HeaderText = "Issue Date";
             dataGridViewInternationalApplications.Columns[5].HeaderText = "Ex. Date";
             dataGridViewInternationalApplications.Columns[6].HeaderText = "Is Active";
-            dataGridViewInternationalApplications.Columns[6].Width = 50;
+            dataGridViewInternationalApplications.Columns[6].Width = 70;
         }
         private void BtnInternationalApplicationsClose_Click(object sender, EventArgs e)
         {
